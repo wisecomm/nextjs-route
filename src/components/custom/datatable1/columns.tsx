f66ba6c7d015b253d9edf55c90362df928d0cc23
danyoh <danyoh@gmail.com>
2025-01-22 18:14:24 +0900

    1

--- src/components/custom/datatable1/columns.tsx
+++ src/components/custom/datatable1/columns.tsx
@@ -4,7 +4,6 @@ import { ColumnDef } from "@tanstack/react-table";
 
 import { Task } from "./data/schema";
 import { DataTableColumnHeader } from "./data-table-column-header";
-import { DataTableRowActions } from "./data-table-row-actions";
 import { Checkbox } from "@/components/ui";
 
 export const columns: ColumnDef<Task>[] = [
@@ -81,9 +80,5 @@ export const columns: ColumnDef<Task>[] = [
         </div>
       );
     },
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <DataTableRowActions row={row} />,
   },
 ];
