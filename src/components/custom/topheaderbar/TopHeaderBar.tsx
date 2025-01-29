6c65a0d76529f32d85526cf7fa8b150fccce1253
danyoh <danyoh#gmail.com>
2025-01-29 14:54:59 +0900

    1

--- src/components/custom/topheaderbar/TopHeaderBar.tsx
+++ src/components/custom/topheaderbar/TopHeaderBar.tsx
@@ -1,20 +1,16 @@
 import Image from "next/image";
 import { ThemeToggle } from "../ThemeToggle";
+import { Label } from "@/components/ui";
 
 const TopHeaderBar = () => {
   return (
     <div className="flex items-center justify-between p-4">
-      {/* SEARCH BAR */}
-      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
-        <Image src="/image/search.png" alt="" width={14} height={14} />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-[200px] p-2 bg-transparent outline-none"
-        />
+      {/* TITLE BAR */}
+      <div className="flex items-center text-9xl font-bold">
+        <Label>Email Anyalyze System V1</Label>
       </div>
       {/* ICONS AND USER */}
-      <div className="flex items-center gap-6 justify-end w-full">
+      <div className="flex items-center gap-6 justify-end">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/image/message.png" alt="" width={20} height={20} />
         </div>
