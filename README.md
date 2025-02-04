2b7ab616e1dad5ac6618b9abc3f12e94962d5332
danyoh <danyoh#gmail.com>
2025-02-04 21:10:12 +0900

    1

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# npm install 에러시 npm install -legacy-peer-deps 
+
+# 처리 사항
+- npm install 에러시 npm install -legacy-peer-deps 
+- 프로젝트 생성 : npx create-next-app@latest src 디렉토리 선택
+
+- middleware.ts 추가 : src 디렉토리 아래 존재
+ > 로그인, 로그인 페이지 제어
+
+- 라우트 그룹 사용
+
+- globals.css 수정 : 메뉴 타이릍 색깔이 흐리게 보여서
 
 
-# 프로젝트 생성 : npx create-next-app@latest src 디렉토리 선택
-
-# middleware.ts 추가 : src 디렉토리 아래 존재
-
-# 라우트 그룹 사용
-
-# globals.css 수정 : 메뉴 타이릍 색깔이 흐리게 보여서
-
-
-#################################################################
 # 참조 사이트 
-#################################################################
-- 메뉴바 
-https://ui.shadcn.com/docs/components/sidebar
-- 참조 샘플
+- 메뉴바   
+ https://ui.shadcn.com/docs/components/sidebar
+- 참조 샘플  
 https://ui.shadcn.com/examples/forms/account
 
-- 그리드
+- 그리드  
 https://ui.shadcn.com/examples/tasks
 
 - 테마 설정 테스트 사이트
--- src/app/(admin)/systemsetting/page.tsx
+++ src/app/(admin)/systemsetting/page.tsx
@@ -4,20 +4,18 @@ import ChildHeaderBar from '@/components/custom/topheaderbar/ChildHeaderBar';
 
 const SystemSettingPage = () => {
   return (
-    <>
-      <div style={{ backgroundColor: 'rgb(232, 234, 244)' }}>
+    <div style={{ backgroundColor: 'rgb(232, 234, 244)' }}>
       <div className="flex flex-1 flex-col gap-4 p-4">
         <ChildHeaderBar />
         <div className="bg-white p-2">
-        <div className="grid auto-rows-min gap-4 md:grid-cols">
-          <div className="aspect-video rounded-xl" >
-            <SystemSettingForm />
+          <div className="grid auto-rows-min gap-4 md:grid-cols">
+            <div className="aspect-video rounded-xl" >
+              <SystemSettingForm />
+            </div>
           </div>
         </div>
-        </div>
-        </div>
-        </div>
-        </>
+      </div>
+    </div>
   );
 };
 
