9fe1644b5dc8521ef2c772b9507c25f169eaaf70
danyoh <danyoh@gmail.com>
2025-02-05 15:04:45 +0900

    1

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,7 +1,7 @@
 import { AppSidebar } from "@/components/custom/appsidebar/app-sidebar";
 import { ThemeProvider } from "@/components/custom/ThemeProvider";
 import TopHeaderBar from "@/components/custom/topheaderbar/TopHeaderBar";
-import { Separator, SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui";
+import { Separator, SidebarInset, SidebarProvider } from "@/components/ui";
 
 export default function RootLayout({
   children,
