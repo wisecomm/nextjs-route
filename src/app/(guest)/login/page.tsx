aad2ffd086931a93319599bf2d5ba294443c9121
danyoh <danyoh#gmail.com>
2025-02-10 21:36:46 +0900

    1

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import Image from "next/image";
 import { setToken } from "@/app/utils/cookie";
 import { useForm } from "react-hook-form";
-import { z } from "zod"
-import { toast } from '@/hooks/use-toast'
+import { z } from "zod";
+import { toast } from "@/hooks/use-toast";
 import { Button, Input, Label } from "@/components/ui";
 import { setLogin } from "@/app/(admin)/actions/useSetLogin";
 
 //import { setLogin } from "@/app/api/useSetLogin";
 
-
 function Login() {
-
   const accountFormSchema = z.object({
     userid: z.string().min(1, {
       message: "사용자 아이디을 입력하세요.",
@@ -21,58 +19,59 @@ function Login() {
     password: z.string().min(4, {
       message: "패스워드는 4자리 이상입니다.",
     }),
-  })
-  type AccountFormValues = z.infer<typeof accountFormSchema>
+  });
+  type AccountFormValues = z.infer<typeof accountFormSchema>;
   const defaultValues: Partial<AccountFormValues> = {
     userid: "홍길동",
-    password: '',
-  }
+    password: "",
+  };
   const formData = useForm<AccountFormValues>({
     defaultValues,
-  })
+  });
 
-  const [isLoading, setIsLoading] = useState(false);
+  const [isPnding, startTransition] = useTransition();
 
-  async function onSubmit(submitData: AccountFormValues) {
-    try {
-      console.log(submitData)
+  const handleSubmit = (submitData: AccountFormValues) => {
+    startTransition(async () => {
+      try {
+        console.log(submitData);
 
-      setIsLoading(true)
+        // 전송 전에 입력필드 검증
+        const result = accountFormSchema.safeParse(submitData);
+        if (!result.success) {
+          const firstError = result.error.errors[0];
+          toast({
+            title: "Validation Error",
+            description: (
+              <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                <code className="text-white">
+                  {JSON.stringify(firstError.message, null, 2)}
+                </code>
+              </pre>
+            ),
+          });
+          return;
+        }
 
-      // 전송 전에 입력필드 검증
-      const result = accountFormSchema.safeParse(submitData)
-      if (!result.success) {
-        const firstError = result.error.errors[0]
-        toast({
-          title: "Validation Error",
-          description: (
-            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-              <code className="text-white">{JSON.stringify(firstError.message, null, 2)}</code>
-            </pre>
-          ),
-        })
-        return
+        /*      
+        const { data, status, error } = await setLogin();
+  
+        console.log("handleLogin data=" + data);
+        console.log("handleLogin status=" + status);
+        console.log("handleLogin error=" + error);
+  
+        setToken(data.key);
+  */
+
+        setToken("test-token-1234567");
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        //      window.location.replace("/main");
+      } catch (error) {
+        console.log("onSubmit error: " + error);
       }
-
-/*      
-      const { data, status, error } = await setLogin();
-
-      console.log("handleLogin data=" + data);
-      console.log("handleLogin status=" + status);
-      console.log("handleLogin error=" + error);
-
-      setToken(data.key);
-*/
-
-      setToken("test-token-1234567");
-      window.location.replace("/main");
-    } catch (error) {
-      console.log("onSubmit error: " + error)
-    } finally {
-      setIsLoading(false)
-    }
-
-  }
+      console.log("startTransition");
+    });
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -87,22 +86,41 @@ function Login() {
           style={{ width: 180, height: 38 }}
           priority
         />
-        <form className="space-y-4" onSubmit={formData.handleSubmit(onSubmit)}>
+        <form className="space-y-4">
           <div>
-            <Label htmlFor="userid" className="block text-sm font-medium text-gray-700">
+            <Label
+              htmlFor="userid"
+              className="block text-sm font-medium text-gray-700"
+            >
               사용자 아이디
             </Label>
-            <Input {...formData.register('userid')} id="userid" placeholder="Enter your userid" />
+            <Input
+              {...formData.register("userid")}
+              id="userid"
+              placeholder="Enter your userid"
+            />
           </div>
           <div>
-            <Label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            <Label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
               패스워드
             </Label>
-            <Input {...formData.register('password')} id="password" placeholder="Enter your password" type="password" />
+            <Input
+              {...formData.register("password")}
+              id="password"
+              placeholder="Enter your password"
+              type="password"
+            />
           </div>
-          <Button type='submit' className="w-full px-4 py-2 font-bold text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200"
-            disabled={isLoading}>
-              {isLoading ? '로그인 중...' : '로그인'}</Button>
+          <Button
+            className="w-full px-4 py-2 font-bold text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200"
+            disabled={isPnding}
+            onClick={formData.handleSubmit(handleSubmit)}
+          >
+            {isPnding ? "로그인 중..." : "로그인"}
+          </Button>
         </form>
       </div>
     </div>
